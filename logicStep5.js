94dd19b74586230f669cbbf850fd0398f833ff93
caseykotowski <79807939+caseykotowski@users.noreply.github.com>
2021-06-18 11:11:23 -0500

    added legend

--- logicStep5.js
+++ logicStep5.js
@@ -101,32 +101,5 @@ let overlays = {
     Earthquakes: earthquakes
 };
 
-// overlay control + legend
-let legend = L.control({
-    position: "bottomright"
-});
-
-//Add details
-legend.onAdd = function() {
-    let div = L.DomUtil.create("div", "info legend");
-    const magnitudes = [0, 1, 2, 3, 4, 5];
-const colors = [
-  "#98ee00",
-  "#d4ee00",
-  "#eecc00",
-  "#ee9c00",
-  "#ea822c",
-  "#ea2c2c"
-];
-// Looping through intervals to generate label with colored square
-    for (var i=0; i< magnitudes.length; i++){
-        console.log(colors[i]);
-        div.innerHTML +=
-        "<i style='background: " + colors[i] + "'></i> " + magnitudes[i] + (magnitudes[i +1] ? "&ndash;" + magnitudes[i +1] + "<br>" : "+");
-    }
-    return div;
-};
-
-legend.addTo(map);
-
-
+// overlay control
+L.control.layers(baseMaps, overlays).addTo(map);
